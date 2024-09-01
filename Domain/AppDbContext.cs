1722cc612abf0ceb192d3649a75c0c4de71958a3
Mihkel Tiganik <mtiganik@gmail.com>
2024-09-01 12:02:45 +0300

    work with domain, before migration

---
+++ Domain/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Domain/Todo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Todo
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100, ErrorMessage = "Title cannot be longer than 100 characters")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = " Description is required")]
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Due Date is required")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Due Date")]
+        public DateTime DueDate { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        [EnumDataType(typeof(Status), ErrorMessage = "Invalid status value")]
+        public Status Status { get; set; }
+
+    }
+}
